1042cf47f7b0dd6e3a9207d058dc2ac7c6b63096
startyuphoney <96329521+startyuphoney@users.noreply.github.com>
2023-07-11 18:19:12 +0900

    Revert "검색까지 완료"
    This reverts commit 690009c84f8ec23b45d4b31554c836ee1608a0b9.

--- sbb/src/main/java/com/mysite/sbb/question/QuestionRepository.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionRepository.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
@@ -12,5 +11,4 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
 	Question findBySubjectAndContent(String subject, String content);
 	List<Question> findBySubjectLike(String subject);
 	Page<Question> findAll(Pageable pageable);
-	Page<Question> findAll(Specification<Question> spec, Pageable pageable);
 }
